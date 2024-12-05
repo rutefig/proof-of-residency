bb885c275f9423fa41514fa37de55ae8fb70dd4f
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2024-12-05 11:03:24 +0700

    messy simple version with sp1 prover network working on the server

--- prover/program/src/main.rs
+++ prover/program/src/main.rs
@@ -9,31 +9,12 @@ sp1_zkvm::entrypoint!(main);
 // inside the zkVM.
 
 pub fn main() {
-    // Read two inputs from the prover: a regex pattern and a target string.
-    // let pattern = sp1_zkvm::io::read::<String>();
-    // let target_string = sp1_zkvm::io::read::<String>();
-    // let pdf_base64 = sp1_zkvm::io::read::<String>();
+    // Read a series of bytes from the input, which should be a PDF file.
     let pdf_bytes = sp1_zkvm::io::read::<Vec<u8>>();
 
     let result = prover_lib::run(&pdf_bytes);
 
     println!("result: {}", result);
-
-    // let pdf_text = pdf_extract::extract_text_from_mem(&pdf_bytes).unwrap();
-
-    // let result = BASE64_STANDARD.decode(pdf_base64.as_bytes()).unwrap();
-
-    // // Try to compile the regex pattern. If it fails, write `false` as output and return.
-    // let regex = match Regex::new(&pattern) {
-    //     Ok(regex) => regex,
-    //     Err(_) => {
-    //         panic!("Invalid regex pattern");
-    //     }
-    // };
-
-    // // Perform the regex search on the target string.
-    // let result = regex.is_match(&target_string);
-    // let result = pdf_text.contains("Lisboa");
 
     // Write the result (true or false) to the output.
     sp1_zkvm::io::commit(&result);
--- prover/script/src/bin/main.rs
+++ prover/script/src/bin/main.rs
@@ -1,44 +1,132 @@
-use sp1_sdk::{include_elf, utils, HashableKey, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
+use bytes::BufMut;
+use futures::{StreamExt, TryStreamExt};
+use sp1_sdk::{
+    include_elf, network::proto::network::ProofMode, utils, HashableKey, NetworkProverV1,
+    ProverClient, SP1ProofWithPublicValues, SP1Stdin,
+};
+use std::convert::Infallible;
+use warp::{
+    filters::multipart::FormData, http::StatusCode, reject::Rejection, reply::Reply, Filter,
+};
 
 /// The ELF we want to execute inside the zkVM.
 const REGEX_IO_ELF: &[u8] = include_elf!("prover-program");
 
-fn main() {
+async fn upload(form: FormData) -> Result<impl Reply, Rejection> {
+    let mut parts = form.into_stream();
+
+    while let Some(Ok(p)) = parts.next().await {
+        if p.name() == "file" {
+            let content_type = p.content_type();
+            match content_type {
+                // for now only pdf files are supported
+                Some(file_type) => match file_type {
+                    "application/pdf" => {
+                        let value = p
+                            .stream()
+                            .try_fold(Vec::new(), |mut vec, data| {
+                                vec.put(data);
+                                async move { Ok(vec) }
+                            })
+                            .await
+                            .map_err(|e| {
+                                eprintln!("reading file error: {}", e);
+                                warp::reject::reject()
+                            })?;
+
+                        // Create a new stdin with d the input for the program.
+                        let mut stdin = SP1Stdin::new();
+
+                        stdin.write(&value);
+
+                        // Generate the proof for the given program and input.
+                        // let client = ProverClient::new();
+                        let network = NetworkProverV1::new();
+                        // println!("Prover client created");
+                        // let (pk, vk) = client.setup(REGEX_IO_ELF);
+                        // println!("vk: {:?}", vk.bytes32());
+                        // let mut proof = client.prove(&pk, stdin).run().expect("proving failed");
+                        let mut proof = network
+                            .prove(REGEX_IO_ELF, stdin, ProofMode::Core, None)
+                            .await
+                            .expect("proving failed");
+
+                        // Read the output.
+                        let res = proof.public_values.read::<bool>();
+                        println!("res: {}", res);
+
+                        // Verify proof.
+                        // client.verify(&proof, &vk).expect("verification failed");
+
+                        // Test a round trip of proof serialization and deserialization. - LATER
+                        // proof
+                        //     .save("proof-with-pis.bin")
+                        //     .expect("saving proof failed");
+                        // let deserialized_proof =
+                        //     SP1ProofWithPublicValues::load("proof-with-pis.bin")
+                        //         .expect("loading proof failed");
+
+                        // // Verify the deserialized proof.
+                        // // client
+                        // //     .verify(&deserialized_proof, &vk)
+                        // //     .expect("verification failed");
+
+                        // println!("successfully generated and verified proof for the program!");
+
+                        let result = proof.public_values.read::<bool>();
+
+                        return Ok(warp::reply::json(&result));
+                    }
+
+                    v => {
+                        eprintln!("invalid file type found: {}", v);
+                        return Err(warp::reject::reject());
+                    }
+                },
+                None => {
+                    eprintln!("file type could not be determined");
+                    return Err(warp::reject::reject());
+                }
+            }
+        }
+    }
+
+    Ok(warp::reply::json(&"success"))
+}
+
+async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let (code, message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if err.find::<warp::reject::PayloadTooLarge>().is_some() {
+        (StatusCode::BAD_REQUEST, "Payload too large".to_string())
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Internal Server Error".to_string(),
+        )
+    };
+
+    Ok(warp::reply::with_status(message, code))
+}
+
+#[tokio::main]
+async fn main() {
     // Setup a tracer for logging.
     utils::setup_logger();
 
-    // Create a new stdin with d the input for the program.
-    let mut stdin = SP1Stdin::new();
+    // Run the server.
+    let upload_route = warp::path("upload")
+        .and(warp::post())
+        .and(warp::multipart::form().max_length(5_000_000))
+        .and_then(upload);
 
-    let file_bytes = std::fs::read("fatura_net.pdf").unwrap();
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_headers(vec!["content-type"])
+        .allow_methods(vec!["POST", "GET"]);
 
-    stdin.write(&file_bytes);
-
-    // Generate the proof for the given program and input.
-    let client = ProverClient::new();
-    println!("Prover client created");
-    let (pk, vk) = client.setup(REGEX_IO_ELF);
-    println!("vk: {:?}", vk.bytes32());
-    let mut proof = client.prove(&pk, stdin).run().expect("proving failed");
-
-    // Read the output.
-    let res = proof.public_values.read::<bool>();
-    println!("res: {}", res);
-
-    // Verify proof.
-    client.verify(&proof, &vk).expect("verification failed");
-
-    // Test a round trip of proof serialization and deserialization.
-    proof
-        .save("proof-with-pis.bin")
-        .expect("saving proof failed");
-    let deserialized_proof =
-        SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
-
-    // Verify the deserialized proof.
-    client
-        .verify(&deserialized_proof, &vk)
-        .expect("verification failed");
-
-    println!("successfully generated and verified proof for the program!")
+    let router = upload_route.recover(handle_rejection).with(cors);
+    println!("Server started at localhost:8080");
+    warp::serve(router).run(([0, 0, 0, 0], 8080)).await;
 }
