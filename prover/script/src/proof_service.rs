31571c6c876e8c94f210a12194c623a32a1f41d1
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2024-12-10 11:53:16 +0700

    handling local and network proving

--- prover/script/src/proof_service.rs
+++ prover/script/src/proof_service.rs
@@ -1,7 +1,8 @@
-use crate::types::ProofResponse;
 use crate::hyle::{Hyle, HyleNetwork};
+use crate::types::ProofResponse;
+use sp1_sdk::network::proto::network::ProofMode;
 use sp1_sdk::{
-    include_elf, HashableKey, ProverClient, SP1Stdin
+    include_elf, HashableKey, NetworkProverV1, ProverClient, SP1ProofWithPublicValues, SP1Stdin,
 };
 use std::path::PathBuf;
 
@@ -12,6 +13,20 @@ pub struct ProofService {
     hyle: Hyle,
 }
 
+enum Prover {
+    Local,
+    Network,
+}
+
+impl Prover {
+    fn from_str(s: &str) -> Self {
+        match s.to_lowercase().as_str() {
+            "network" => Prover::Network,
+            _ => Prover::Local, // Default to Local for any other value
+        }
+    }
+}
+
 impl ProofService {
     pub fn new(base_path: &PathBuf) -> Self {
         Self {
@@ -19,9 +34,12 @@ impl ProofService {
         }
     }
 
-    pub async fn generate_proof(&self, file_content: Vec<u8>) -> Result<ProofResponse, Box<dyn std::error::Error>> {
+    pub async fn generate_proof(
+        &self,
+        file_content: Vec<u8>,
+    ) -> Result<ProofResponse, Box<dyn std::error::Error>> {
         let mut stdin = SP1Stdin::new();
-        
+
         let null_state = 0u32.to_be_bytes().to_vec();
         let hyle_input = self.hyle.publish_payload(
             "default",
@@ -33,14 +51,17 @@ impl ProofService {
 
         stdin.write(&hyle_input);
 
-        // Local prover implementation
-        let client = ProverClient::new();
-        let (pk, vk) = client.setup(REGEX_IO_ELF);
-        println!("vk: {:?}", vk.bytes32());
-        
-        let mut proof = client.prove(&pk, stdin).run()?;
+        let prover = std::env::var("SP1_PROVER")
+            .map(|s| Prover::from_str(&s))
+            .unwrap_or(Prover::Local);
+
+        let mut proof = match prover {
+            Prover::Local => self.generate_proof_local(stdin),
+            Prover::Network => self.generate_proof_network(stdin).await,
+        };
+
         let verification_result = proof.public_values.read::<bool>();
-        
+
         // Save proof to file
         proof.save("proof-with-pis.bin")?;
         let proof_bytes = std::fs::read("proof-with-pis.bin")?;
@@ -50,5 +71,27 @@ impl ProofService {
             result: verification_result,
             proof: proof_bytes,
         })
+    }
+
+    fn generate_proof_local(&self, stdin: SP1Stdin) -> SP1ProofWithPublicValues {
+        let client = ProverClient::new();
+        let (pk, vk) = client.setup(REGEX_IO_ELF);
+        println!("vk: {:?}", vk.bytes32());
+
+        let proof = client.prove(&pk, stdin).run().unwrap();
+
+        proof
+    }
+
+    async fn generate_proof_network(&self, stdin: SP1Stdin) -> SP1ProofWithPublicValues {
+        let network = NetworkProverV1::new_from_key(
+            &std::env::var("SP1_NETWORK_KEY").expect("SP1_NETWORK_KEY not set"),
+        );
+        let proof = network
+            .prove(REGEX_IO_ELF, stdin, ProofMode::Core, None)
+            .await
+            .unwrap();
+
+        proof
     }
 }
